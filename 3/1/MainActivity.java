be20ec568f7b656c1c1460dac6c723fdb1e67651
ccihxn <wjswlghks77@naver.com>
2023-10-18 01:43:51 +0900

    003_01

---
+++ 3/1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+        button.setOnClickListener(v -> {
+            String answer = "정답 : 서울";
+            textView.setText(answer);
+        });
+    }
+}
